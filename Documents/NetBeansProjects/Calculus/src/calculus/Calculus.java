c90c09fc3a883d9b82e8554527c080ccc33ce501
Sen <>
2017-10-25 09:38:04 +0200

    test

---
+++ Documents/NetBeansProjects/Calculus/src/calculus/Calculus.java
@@ -0,0 +1,43 @@
+
+package calculus;
+
+/**
+ * 
+ * @author @author Ping Tian-sen
+ */
+public class Calculus {
+    
+    
+    public int addition (int a, int b){
+    
+       return (a+b);
+    
+    }
+    
+    public int multiplication (int a, int b) {
+    
+        return (a*b);
+    
+    }
+    
+    public int soustraction (int a, int b){
+    
+        return (a-b);
+    
+    }
+    
+    
+    public double division (int a,int b){
+    
+        return (a/b);
+    
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Documents/NetBeansProjects/Calculus/test/CalculusJunit.java
@@ -0,0 +1,76 @@
+
+import calculus.Calculus;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author @author Ping Tian-sen
+ */
+public class CalculusJunit {
+    
+    public CalculusJunit() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+   
+    @Test 
+    public void multiplicationTest(){
+    
+        Calculus c = new Calculus ();
+        
+        assertEquals(" integer X 0 doit donner 0",0,c.multiplication(10, 0));
+        assertEquals(" 0 X integer doit donner 0",0,c.multiplication(0, 10));
+        assertEquals(" 0 X 0 doit donner 0",0,c.multiplication(0, 0));
+        assertEquals(" 10 X 10 doit donner 100",100,c.multiplication(10, 10));
+        assertEquals(" -1 X 10 doit donner -10",-10,c.multiplication(-1, 10));
+    }
+    
+    
+     @Test(expected = java.lang.ArithmeticException.class)
+    public void divisionPar0Test(){
+    
+        Calculus c = new Calculus ();
+        
+        c.division(10, 0);
+        
+    }
+    
+    
+    @Test
+    public void additionTest(){
+        Calculus c = new Calculus ();
+   
+        assertEquals(" 1 + 0 doit donner 1",0,c.addition(0, 0));
+
+    
+    }
+    
+     @Test
+    public void soustractionTest(){
+        Calculus c = new Calculus ();
+   
+        assertEquals(" 1 - 1 doit donner 0",0,c.soustraction(1, 1));
+
+    
+    }
+   
+}
